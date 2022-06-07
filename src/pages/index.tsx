032f760b57218f49158446eb4754c6f9007380c9
Niko Lindroos <niko.j.makela@gmail.com>
2022-06-07 15:43:25 +0300

    style: fixed the break points usage and the front page layout styles
    HH-56 HH-64 HH-90

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,23 @@
 import React from "react";
 import { GetStaticPropsContext } from "next";
 import { useRouter } from "next/router";
-import { gql, useQuery } from "@apollo/client";
-import { useTranslation } from "next-i18next";
+import { useQuery } from "@apollo/client";
 import { Navigation, Page } from "react-helsinki-headless-cms/apollo";
 
 import getHobbiesStaticProps from "../domain/app/getHobbiesStaticProps";
 import serverSideTranslationsWithCommon from "../domain/i18n/serverSideTranslationsWithCommon";
 import { getLocaleOrError } from "../domain/i18n/router/utils";
 import { getQlLanguage } from "../common/apollo/utils";
-import Section from "../common/components/section/Section";
-import Hero from "../common/components/hero/Hero";
-import HeroImage from "../common/components/hero/HeroImage";
-import SearchShortcuts from "../common/components/searchShortcuts/SearchShortcuts";
-import getCurrentSeason from "../domain/season/getCurrentSeason";
-import shortcuts from "../domain/shortcuts/shortcutsData";
-import LandingPageSearch from "../domain/landingPageSearch/LandingPageSearch";
-
-export const LANDING_PAGE_QUERY = gql`
-  query LandingPageQuery($languageCode: LanguageCodeEnum!) {
-    landingPage(id: "root", idType: SLUG) {
-      id
-      desktopImage {
-        edges {
-          node {
-            mediaItemUrl
-          }
-        }
-      }
-      translation(language: $languageCode) {
-        title
-        description
-        heroLink
-      }
-    }
-  }
-`;
+import {
+  LandingPageMainContent,
+  LANDING_PAGE_QUERY,
+} from "../domain/landingPage/LandingPage";
+import styles from "./pageLayout.module.scss";
 
 export default function HomePage() {
-  const { t } = useTranslation("home_page");
-  const { t: tShortcuts } = useTranslation("hardcoded_shortcuts");
   const router = useRouter();
   const currentPage = router.pathname;
-  const currentSeason = getCurrentSeason();
+
   const language = getQlLanguage(router.locale ?? router.defaultLocale);
   const { data } = useQuery(LANDING_PAGE_QUERY, {
     variables: {
@@ -50,63 +25,21 @@ export default function HomePage() {
     },
   });
 
-  const landingPage = data?.landingPage?.translation;
-
-  const heroImage =
-    data?.landingPage?.desktopImage?.edges[0]?.node?.mediaItemUrl;
-
-  const content = (
-    <main>
-      {landingPage && (
-        <>
-          <HeroImage desktopImageUri={heroImage} />
-          <Section variant="contained" color="transparent">
-            <Hero
-              title={landingPage.title}
-              description={landingPage.description}
-              cta={{
-                label: landingPage.heroLink[0],
-                href: landingPage.heroLink[1],
-              }}
-            />
-          </Section>
-        </>
-      )}
-      <Section color="transparent">
-        <LandingPageSearch />
-        <SearchShortcuts
-          shortcuts={shortcuts
-            .filter((shortcut) => shortcut.seasons.includes(currentSeason))
-            .map((shortcut) => ({
-              id: shortcut.id,
-              label: tShortcuts(shortcut.id),
-              icon: shortcut.icon,
-              ontologyTreeIds: shortcut.ontologyTreeIds,
-            }))}
-        />
-      </Section>
-      <Section title={t("recommended_collections_title")}>
-        TODO: Collection grid
-      </Section>
-    </main>
-  );
-
   return (
     <Page
       uri="/"
+      className={styles.pageLayout}
       navigation={
-        <header>
-          <Navigation
-            menuName="Hobbies Helsinki Header"
-            onTitleClick={() => {
-              // eslint-disable-next-line no-console
-            }}
-            getIsItemActive={({ path }) => path === currentPage}
-            getPathnameForLanguage={() => currentPage}
-          />
-        </header>
+        <Navigation
+          menuName="Hobbies Helsinki Header"
+          onTitleClick={() => {
+            // eslint-disable-next-line no-console
+          }}
+          getIsItemActive={({ path }) => path === currentPage}
+          getPathnameForLanguage={() => currentPage}
+        />
       }
-      content={content}
+      content={<LandingPageMainContent page={data?.landingPage} />}
       footer={<footer>TODO: footer</footer>}
     />
   );
