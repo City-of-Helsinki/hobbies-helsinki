992cfe76638d31c910b6a02030842869f3e089eb
Niko Lindroos <niko.j.makela@gmail.com>
2022-06-10 22:40:35 +0300

    style: use blue-coloured theme

--- src/domain/search/landingPage/LandingPage.tsx
+++ src/domain/search/landingPage/LandingPage.tsx
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
+import { Koros } from "hds-react";
 import { useTranslation } from "next-i18next";
+import { PageContentLayoutProps } from "react-helsinki-headless-cms/";
 
 import Hero from "../../../common/components/hero/Hero";
 import HeroImage from "../../../common/components/hero/HeroImage";
@@ -32,52 +34,59 @@ export const LANDING_PAGE_QUERY = gql`
 
 export type LandingPageProps = {
   // TODO: Fix any type by adding the landing page query to HCRC-lib
-  page: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  landingPage?: any;
 };
 
-export function LandingPageMainContent({ page }: LandingPageProps) {
-  const landingPage = page?.translation;
-  const heroImage = page?.desktopImage?.edges[0]?.node?.mediaItemUrl;
+export function LandingPageContentLayout({
+  landingPage,
+  collections,
+}: LandingPageProps & PageContentLayoutProps) {
+  const { title, description, heroLink } = landingPage?.translation;
+  const heroImage = landingPage?.desktopImage?.edges[0]?.node?.mediaItemUrl;
   const currentSeason = getCurrentSeason();
   const { t } = useTranslation();
   const { t: tShortcuts } = useTranslation("hardcoded_shortcuts");
   return (
     <div className={styles.layout}>
       <main className={styles.main}>
-        {landingPage && (
-          <>
-            <HeroImage desktopImageUri={heroImage} />
-            <Section variant="contained" color="transparent">
-              <Hero
-                title={landingPage.title}
-                description={landingPage.description}
-                cta={{
-                  label: landingPage.heroLink[0],
-                  href: landingPage.heroLink[1],
-                }}
-              />
-            </Section>
-          </>
-        )}
-        <Section color="transparent">
-          <LandingPageSearch />
-          <SearchShortcuts
-            shortcuts={shortcuts
-              .filter((shortcut) =>
-                currentSeason !== null
-                  ? shortcut.seasons.includes(currentSeason)
-                  : true
-              )
-              .map((shortcut) => ({
-                id: shortcut.id,
-                label: tShortcuts(shortcut.id),
-                icon: shortcut.icon,
-                ontologyTreeIds: shortcut.ontologyTreeIds,
-              }))}
-          />
-        </Section>
-        <Section title={t("recommended_collections_title")}>
-          TODO: Collection grid
+        <div className={styles.highlight}>
+          {landingPage?.translation && (
+            <>
+              <HeroImage desktopImageUri={heroImage} />
+              <Section variant="contained" color="transparent">
+                <Hero
+                  title={title}
+                  description={description}
+                  cta={{
+                    label: heroLink[0],
+                    href: heroLink[1],
+                  }}
+                />
+              </Section>
+            </>
+          )}
+          <Section color="transparent">
+            <LandingPageSearch />
+            <SearchShortcuts
+              shortcuts={shortcuts
+                .filter((shortcut) =>
+                  currentSeason !== null
+                    ? shortcut.seasons.includes(currentSeason)
+                    : true
+                )
+                .map((shortcut) => ({
+                  id: shortcut.id,
+                  label: tShortcuts(shortcut.id),
+                  icon: shortcut.icon,
+                  ontologyTreeIds: shortcut.ontologyTreeIds,
+                }))}
+            />
+          </Section>
+        </div>
+        <Koros className={styles.koros} flipHorizontal />
+        <Section color="transparent" title={t("recommended_collections_title")}>
+          {collections}
         </Section>
       </main>
     </div>
