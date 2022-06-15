531278b102b5e701c9e3d83ad0243ae91b4c2da5
Niko Lindroos <niko.j.makela@gmail.com>
2022-06-15 15:13:10 +0300

    feat: create collection item url with RHHC config utils
    HH-94

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,23 @@ import { GetStaticPropsContext } from "next";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import {
+  ArticleQuery,
   Page as RHHCApolloPage,
   PageContent as RHHCApolloPageContent,
+  PageQuery,
 } from "react-helsinki-headless-cms/apollo";
+import {
+  Card,
+  CardProps,
+  Collection,
+  CollectionItemType,
+  CollectionType,
+  getCollections,
+  getElementTextContent,
+  isLayoutPage,
+  ModuleItemTypeEnum,
+  useConfig,
+} from "react-helsinki-headless-cms";
 
 import getHobbiesStaticProps from "../domain/app/getHobbiesStaticProps";
 import serverSideTranslationsWithCommon from "../domain/i18n/serverSideTranslationsWithCommon";
@@ -19,9 +33,57 @@ import { DEFAULT_LANGUAGE } from "../constants";
 import Navigation from "../common-events/components/navigation/Navigation";
 import FooterSection from "../domain/footer/Footer";
 
+export const getCollectionCard = (
+  item: CollectionItemType,
+  defaultImageUrl?: string
+): CardProps => ({
+  id: item?.id,
+  title: item?.title ?? "",
+  url: item?.link ?? "#",
+  imageUrl: item?.featuredImage?.node?.mediaItemUrl || defaultImageUrl,
+  ariaLabel: item?.title ?? "",
+  className: undefined,
+  imageLabel: item?.featuredImage?.node?.title ?? "",
+  subTitle: undefined,
+  text: getElementTextContent((item?.lead || item?.content) ?? ""),
+  customContent: undefined,
+  hasLink: true,
+  withBorder: false,
+  withShadow: true,
+  clampText: true,
+  direction: "responsive" as CardProps["direction"],
+  target: "_self" as CardProps["target"],
+});
+
+export const defaultCollections = (
+  page: PageQuery["page"] | ArticleQuery["post"],
+  getRoutedInternalHref: (link: string, type: ModuleItemTypeEnum) => string
+) =>
+  getCollections(page?.modules as CollectionType[])?.map((collection) => {
+    const cards = collection.items.map((item) => {
+      const itemType = isLayoutPage(item)
+        ? ModuleItemTypeEnum.Page
+        : ModuleItemTypeEnum.Article;
+      const cardProps = getCollectionCard(item);
+      const url = getRoutedInternalHref(item?.link as string, itemType);
+      return <Card key={item?.id} {...cardProps} url={url} />;
+    });
+    return (
+      <Collection
+        key={`collection-${Math.random()}`}
+        title={collection.title}
+        cards={cards}
+        type="grid"
+        collectionContainerProps={{ withDots: false }}
+      />
+    );
+  });
+
 export default function HomePage() {
   const router = useRouter();
-
+  const {
+    utils: { getRoutedInternalHref },
+  } = useConfig();
   const language = getQlLanguage(
     router.locale ?? router.defaultLocale ?? DEFAULT_LANGUAGE
   );
@@ -44,6 +106,9 @@ export default function HomePage() {
           ]}
           landingPage={data.landingPage}
           PageContentLayoutComponent={LandingPageContentLayout}
+          collections={(page) =>
+            defaultCollections(page, getRoutedInternalHref)
+          }
         />
       }
       footer={<FooterSection />}
