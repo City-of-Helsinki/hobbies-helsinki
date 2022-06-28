b778cc42884d5cf99997fe4bfd585fe3806cfb79
Niko Lindroos <niko.j.makela@gmail.com>
2022-06-28 13:37:31 +0300

    test: fix textFilter
    HH-95

--- src/domain/search/eventSearch/filterSummary/__tests__/TextFilter.test.tsx
+++ src/domain/search/eventSearch/filterSummary/__tests__/TextFilter.test.tsx
@@ -1,28 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import React from 'react';
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { act, render, screen } from "../../../../../tests/testUtils";
 
-import TextFilter from '../TextFilter';
+import TextFilter from "../TextFilter";
 
 const props = {
   onRemove: jest.fn(),
-  text: 'text',
+  text: "text",
 };
 
-it('matches snapshot', () => {
+it("matches snapshot", () => {
   const { container } = render(<TextFilter {...props} />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-it('calls onRemove callback when remove button is clicked', () => {
+it("calls onRemove callback when remove button is clicked", async () => {
   const onClickMock = jest.fn();
   render(<TextFilter {...props} onRemove={onClickMock} />);
 
   expect(screen.queryByText(`${props.text}`)).toBeInTheDocument();
 
-  userEvent.click(screen.getByRole('button'));
+  await act(() => userEvent.click(screen.getByRole("button")));
 
   expect(onClickMock).toHaveBeenCalled();
-  expect(onClickMock).toHaveBeenCalledWith(props.text, 'text');
+  expect(onClickMock).toHaveBeenCalledWith(props.text, "text");
 });
