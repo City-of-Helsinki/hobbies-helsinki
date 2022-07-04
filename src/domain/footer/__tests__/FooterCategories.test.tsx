f020ffca837b0cd89dfe71bc854678d5b25772b6
Niko Lindroos <niko.j.makela@gmail.com>
2022-07-04 13:36:19 +0300

    test: fix footer categories
    HH-95

--- src/domain/footer/__tests__/FooterCategories.test.tsx
+++ src/domain/footer/__tests__/FooterCategories.test.tsx
@@ -1,35 +1,39 @@
-import i18n from 'i18next';
-import { axe } from 'jest-axe';
-import React from 'react';
+import i18n from "i18next";
+import { axe } from "jest-axe";
+import React from "react";
 
-import { render, screen, userEvent } from '../../../../test/testUtils';
-import { ROUTES } from '../../routes/constants';
-import FooterCategories from '../FooterCategories';
+import { screen, render, userEvent, act } from "../../../tests/testUtils";
+import FooterCategories from "../FooterCategories";
 
 beforeEach(() => {
-  i18n.changeLanguage('fi');
+  i18n.changeLanguage("fi");
 });
 
-test('component should be accessible', async () => {
-  const { container } = render(<FooterCategories route={ROUTES.EVENTS} />);
+test("component should be accessible", async () => {
+  const { container } = render(<FooterCategories />);
 
   expect(await axe(container)).toHaveNoViolations();
 });
 
-test('should route to event search page by clicking category', () => {
-  const { history } = render(<FooterCategories route={ROUTES.EVENTS} />);
+test("should route to event search page by clicking category", async () => {
+  const { router } = render(<FooterCategories />);
 
-  userEvent.click(screen.getByRole('link', { name: /elokuva/i }));
+  await act(() =>
+    userEvent.click(screen.getByRole("link", { name: /musiikki/i }))
+  );
 
-  expect(history.location.pathname).toMatchSnapshot();
+  expect(router.pathname).toMatchSnapshot();
 });
 
 //  TODO: It seems that hds Footer does not support logoLanguage yet
-test.skip('should show Swedish logo', () => {
-  i18n.changeLanguage('sv');
-  render(<FooterCategories route={ROUTES.EVENTS} />);
+test.todo("should show Swedish logo");
+// test("should show Swedish logo", async () => {
+//   i18n.changeLanguage("sv");
+//   render(<FooterCategories />);
 
-  userEvent.click(screen.getByRole('button', { name: /elokuva/i }));
+//   await act(() =>
+//     userEvent.click(screen.getByRole("link", { name: /musik/i }))
+//   );
 
-  expect(screen.queryByRole('img')).toHaveClass('sv');
-});
+//   expect(screen.queryByRole("img")).toHaveClass("sv");
+// });
