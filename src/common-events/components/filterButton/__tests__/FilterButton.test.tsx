2b44aee0415dc00bbb1e461fe81afd174aa9efa4
Niko Lindroos <niko.j.makela@gmail.com>
2022-07-05 08:18:44 +0300

    test: fix filter button
    HH-95

--- src/common-events/components/filterButton/__tests__/FilterButton.test.tsx
+++ src/common-events/components/filterButton/__tests__/FilterButton.test.tsx
@@ -1,6 +1,6 @@
-import { render } from '@testing-library/react';
 import * as React from 'react';
 
+import { render } from '../../../../tests/testUtils';
 import FilterButton from '../FilterButton';
 
 test('matches snapshot', () => {
