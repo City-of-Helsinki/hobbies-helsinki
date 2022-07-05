467b6d189513aee88ad2ffe73ba3dd254c3fecd8
Niko Lindroos <niko.j.makela@gmail.com>
2022-07-05 08:35:22 +0300

    test: share links
    HH-95

--- src/common-events/components/shareLinks/__tests__/ShareLinks.test.tsx
+++ src/common-events/components/shareLinks/__tests__/ShareLinks.test.tsx
@@ -1,6 +1,6 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 
+import { render } from '../../../../tests/testUtils';
 import ShareLinks, { ShareLinksProps } from '../ShareLinks';
 
 const renderComponent = (props: ShareLinksProps) =>
