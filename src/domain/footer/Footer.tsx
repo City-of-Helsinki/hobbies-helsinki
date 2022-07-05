3c20281071a924cbe69af4abd5bae34f3f49f5ef
Niko Lindroos <niko.j.makela@gmail.com>
2022-07-05 08:30:15 +0300

    fix: footer link

--- src/domain/footer/Footer.tsx
+++ src/domain/footer/Footer.tsx
@@ -3,13 +3,15 @@ import { useTranslation } from 'next-i18next';
 import React, { FunctionComponent } from 'react';
 
 import { resetFocusId } from '../../common-events/components/resetFocus/ResetFocus';
+import useLocale from '../../common-events/hooks/useLocale';
+import { getI18nPath } from '../../common-events/i18n/router/utils';
 import styles from './footer.module.scss';
 import FooterCategories from './FooterCategories';
 
 const FooterSection: FunctionComponent = () => {
   const { t } = useTranslation('footer');
   const { t: tCommon } = useTranslation('common');
-
+  const locale = useLocale();
   // override Footer component default behaviour which focuses skip-link
   const handleBackToTop = () => {
     window?.scrollTo({ top: 0 });
@@ -19,7 +21,11 @@ const FooterSection: FunctionComponent = () => {
   return (
     <Footer title={tCommon('appName')} className={styles.footer}>
       <Footer.Navigation>
-        <Footer.Item as={Link} label={t('searchHobbies')} href={'/search'} />
+        <Footer.Item
+          as={Link}
+          label={t('searchHobbies')}
+          href={getI18nPath('/search', locale)}
+        />
       </Footer.Navigation>
       <FooterCategories />
       <Footer.Utilities
