69300b09841890aca38ce2ba153cf6fb705c59db
Niko Lindroos <niko.j.makela@gmail.com>
2022-09-15 11:58:34 +0300

    refactor(similar events): use collection and cards from the HCRC
    HH-83
    fix: add a loading spinner around the similar events component

--- src/domain/event/similarEvents/SimilarEvents.tsx
+++ src/domain/event/similarEvents/SimilarEvents.tsx
@@ -1,44 +1,77 @@
 import { useTranslation } from 'next-i18next';
 import React from 'react';
+import {
+  Card,
+  getEventCardProps,
+  useConfig,
+  Collection,
+  CollectionProps,
+  PageSection,
+  ContentContainer,
+} from 'react-helsinki-headless-cms';
 
-import Container from '../../../common-events/components/layout/Container';
+import useLocale from '../../../common-events/hooks/useLocale';
 import LoadingSpinner from '../../../common/components/spinner/LoadingSpinner';
-// Use same page size as on event search page
-import EventCard from '../eventCard/EventCard';
+import { ROUTES } from '../../../constants';
+import { getLocalizedCmsItemUrl } from '../../../utils/routerUtils';
 import { useSimilarEventsQuery } from '../queryUtils';
 import { EventFields } from '../types';
 import styles from './similarEvents.module.scss';
 
 interface Props {
   event: EventFields;
+  type?: CollectionProps['type'];
 }
 
 export const similarEventsListTestId = 'similar-events-list';
 
-const SimilarEvents: React.FC<Props> = ({ event }) => {
+const SimilarEvents: React.FC<Props> = ({ event, type = 'carousel' }) => {
   const { t } = useTranslation('event');
+  const locale = useLocale();
   const { data: events, loading } = useSimilarEventsQuery(event);
+  const {
+    components: { EventCardContent },
+  } = useConfig();
+
+  const cards = events.map((event, i) => {
+    const cardProps = getEventCardProps(event, locale);
+    const url = getLocalizedCmsItemUrl(
+      ROUTES.COURSES,
+      { eventId: event.id },
+      locale
+    );
+    return (
+      <Card
+        key={cardProps.id}
+        {...cardProps}
+        url={url}
+        direction="fixed-vertical"
+        customContent={
+          EventCardContent && <EventCardContent event={events[i]} />
+        }
+      />
+    );
+  });
 
   return (
-    <div className={styles.similarEvents}>
-      <LoadingSpinner hasPadding={false} isLoading={loading}>
-        {!!events?.length && (
-          <Container>
-            <h2 className={styles.similarEventsTitle}>
-              {t('similarEvents.title')}
-            </h2>
-            <div
-              className={styles.similarEventList}
-              data-testid={similarEventsListTestId}
-            >
-              {events.map((item) => {
-                return <EventCard key={item.id} event={item} />;
-              })}
-            </div>
-          </Container>
-        )}
-      </LoadingSpinner>
-    </div>
+    <PageSection
+      korosTop
+      korosTopClassName={styles.korosTopCollections}
+      className={styles.similarEvents}
+    >
+      <ContentContainer>
+        {/* TODO: the loading should be indicated from inside of the Collection component instead, 
+        so that the title would be shown while the content is being loaded and ther ewould be less bouncing. */}
+        <LoadingSpinner isLoading={loading}>
+          <Collection
+            type={type}
+            title={t('similarEvents.title')}
+            cards={cards}
+            loading={loading}
+          />
+        </LoadingSpinner>
+      </ContentContainer>
+    </PageSection>
   );
 };
 
